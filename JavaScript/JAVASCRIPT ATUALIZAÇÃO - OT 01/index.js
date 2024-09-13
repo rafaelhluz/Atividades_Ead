ee5e97c63d4c2eb549b3bef50a3e4e714f5d94df
rafa <rafaelhluz11@gmail.com>
2024-09-12 21:20:01 -0300

    Add files via upload

---
+++ JavaScript/JAVASCRIPT ATUALIZAÇÃO - OT 01/index.js
@@ -0,0 +1,14 @@
+/*Exercícios com variáveis
+1. Crie 3 variáveis contendo:
+● Seu nome;
+● Sua idade;
+● Comida favorita;
+Após declarar as variáveis e atribuir os valores, utilize o console.log para
+formar uma frase como:
+“Meu nome é …, tenho …. anos e minha comida favorita é …..*/
+
+const nome = "Rafael";
+const idade = 19;
+const comidaFav = "pizza";
+
+console.log("Meu nome é " + nome + ", tenho " + idade + " anos e minha comida favorita é " + comidaFav);
